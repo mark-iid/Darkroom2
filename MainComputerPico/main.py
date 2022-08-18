fd220859ab48b85e1660a0a7599ab62e3a6376b7
Mark Earnest <mark@identityworksllc.com>
2022-08-18 14:20:49 -0400

    Add main computer pico code

---
+++ MainComputerPico/main.py
@@ -0,0 +1,40 @@
+from machine import Pin
+import time
+import sys
+
+led = Pin(25, Pin.OUT)
+
+def set_bit(position, binary):
+    bit_mask = 1 << position
+    return bit_mask | binary
+
+output_line1 = Pin(0, Pin.OUT)
+input_line1 = Pin(1, Pin.IN, Pin.PULL_DOWN)
+output_line2 = Pin(2, Pin.OUT)
+input_line2 = Pin(3, Pin.IN, Pin.PULL_DOWN)
+output_line3 = Pin(4, Pin.OUT)
+input_line3 = Pin(5, Pin.IN, Pin.PULL_DOWN)
+output_line4 = Pin(6, Pin.OUT)
+input_line4 = Pin(7, Pin.IN, Pin.PULL_DOWN)
+led.value(1)
+
+while True:
+    mask = 0
+    output_line1.value(1)
+    output_line2.value(1)    
+    output_line3.value(1)    
+    output_line4.value(1)
+    if(input_line1.value()):
+        mask = set_bit(0, mask)
+    if(input_line2.value()):
+        mask = set_bit(1, mask)
+    if(input_line3.value()):
+        mask = set_bit(2, mask)
+    if(input_line4.value()):
+        mask = set_bit(3, mask)
+    print(mask)
+    while True:
+        v = sys.stdin.readline().strip()
+        if v.lower() == "getbytes":
+            break
+
